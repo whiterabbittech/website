52fad9fa9416d0b63cafbbe6504be44db4890f06
Robbie McKinstry <thesnowmancometh@gmail.com>
2023-02-07 13:53:11 -0500

    CTA copy update

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -4,6 +4,12 @@ import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import backgroundImage from '@/images/background-call-to-action.jpg'
 
+const copy = {
+  primary_cta: "Get started today",
+  cta_subtext: "You'll go from zero to 🚀 in no time.",
+  button_cta: "Let's chat.",
+};
+
 export function CallToAction() {
   return (
     <section
@@ -21,14 +27,13 @@ export function CallToAction() {
       <Container className="relative">
         <div className="mx-auto max-w-lg text-center">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
-            Get started today
+            {copy.primary_cta}
           </h2>
           <p className="mt-4 text-lg tracking-tight text-white">
-            It’s time to take control of your books. Buy our software so you can
-            feel like you’re doing something productive.
+            {copy.cta_subtext}
           </p>
           <Button href="/register" color="white" className="mt-10">
-            Get 6 months free
+            {copy.button_cta}
           </Button>
         </div>
       </Container>
